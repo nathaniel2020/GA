88029f90194b029bd1aa7dd985b2cb504ebfbbf6
nathaniel <nathaniel2014@163.com>
2021-01-25 09:41:53 +0800

    test element-ui

---
+++ src/pages/classify/Classify.vue
@@ -0,0 +1,17 @@
+<template>
+  <classify-input></classify-input>
+</template>
+
+<script>
+import ClassifyInput from './components/Input'
+export default {
+  name: 'Classify',
+  components: {
+    ClassifyInput
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/classify/components/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 2, maxRows: 4}"
+      placeholder="请输入内容"
+      v-model="textarea2">
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClassifyInput',
+  data () {
+    return {
+      textarea2: '1111'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
